5b291aed33bb23449fa183d08afcdda005ee9423
Kiswari10 <kiswari10@gmail.com>
2020-05-18 02:29:04 -0500

    add searchPokemon function and chalk

---
+++ cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const searchPokemon = require('./index');
+
+const args = process.argv[2]
+
+searchPokemon(args) 
+
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const Pokedex = require('pokedex-promise-v2');
 const pokemon = new Pokedex();
+const chalk = require('chalk');
+const log = console.log;
 
-pokemon.getPokemonByName('eevee') // with Promise
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log('There was an ERROR: ', error);
+const searchPokemon = (value) => {
+  return pokemon.getPokemonByName(value)
+  .then((res)=> {
+    let types = [];
+    res.types.forEach(element => {
+      types.push(element.type.name)
     });
+    let abilities = [];
+    res.abilities.forEach(element => {
+      abilities.push(element.ability.name)
+    });
+    log('')
+    log(chalk.gray.underline.bold('Información del Pokemon seleccionado'))
+    log('')
+    log(chalk.cyan('Pokémon: ') + chalk.white(res.name));
+    log(chalk.cyan('id: ') + chalk.white(res.id));
+    log(chalk.cyan('tipos: ') + chalk.white(types.join(', ')));
+    log(chalk.cyan('poderes: ') + chalk.white(abilities.join(', ')));
+})
+}
+
+module.exports = searchPokemon;
